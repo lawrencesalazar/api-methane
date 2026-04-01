ad7af02463dca018c65fdfe3b7bee90a116f8c1a
Lawrence Salazar <asterew20@gmail.com>
2026-04-01 14:03:47 +0800

    update functions

--- main.py
+++ main.py
@@ -239,3 +239,11 @@ def api_alerts(sensor_id: str):
 @app.get("/api/visualization/chart/{sensor_id}")
 def api_chart(sensor_id: str):
     return get_chart(sensor_id)
+
+
+# ===========
+# Run the application
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
